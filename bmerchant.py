8d24cfe544fc5883a6f1948e452a37757f69338e
Jay Wilner <info@outofboundscommunications.com>
2016-12-29 17:38:32 -0600

    changes

--- bmerchant.py
+++ bmerchant.py
@@ -13,7 +13,7 @@ import string
 
 fname = raw_input('Enter the file name: ')
 if len(fname)==0:
-    fname = "GoogleMerchantCenterfeed.csv"
+    fname = "GoogleMerchantCenterfeedUpdatedPrices.csv"
 try:
     fhand = open(fname)
 except:
@@ -22,22 +22,24 @@ except:
 
 #define list
 products = []
-
-rowCounter = 0
-#read in each line of csv file
+firstline = True
+lineCount = 0
+#read in each line of raw csv or .txt product file
 for line in fhand:
-    line = line.rstrip()
-    #split the line into words at each comma
-    words = line.split(",")
-    #reset word counter
-    counter = 0
-    #if length of row is zero, its blank, so skip
-    if len(words)==0:
+    #code for how to skip header row
+    if firstline:
+        firstline = False
         continue
-    #for each field in the row, print out the string
-    for word in words:
-        print word
-        counter = counter +1
-    #increment the rowCounter by 1
-    rowCounter = rowCounter +1
-    print 'the number of values/words in this line is: ', counter
+    #lets just process the first 10 lines for now
+    if lineCount <10:
+        line = line.rstrip()
+        #split the line into words at each comma
+        words = line.split(",")
+        #if row is blank, skip
+        if len(words)==0:
+            continue
+        #for each field in the row from the input file), print out the string
+        for word in words:
+            print word,
+    lineCount = lineCount + 1
+    print 'the line we just finished processing is: ', lineCount
